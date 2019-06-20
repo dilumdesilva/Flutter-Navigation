8e02f38b61704e8aec89d210693c5c0d556b0ec8
Dilum De Silva <dilumd@zone24x7.com>
2019-06-20 13:43:43 +0530

    added named routing related codebase

--- code_samples/simple_navigation/lib/main.dart
+++ code_samples/simple_navigation/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:simple_navigation/page1.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +11,70 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+
+     routes: <String, WidgetBuilder>{
+        '/page1': (BuildContext context) => new Page1(),
+        '/page2': (BuildContext context) => new Page2(),
+     },
+
       home: Page1()
     );
   }
 }
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    backgroundColor: Colors.blue,
+    floatingActionButton: FloatingActionButton(
+      onPressed: (){
+        Navigator.of(context).pushNamed('/page2');
+      },
+      tooltip:'Next Page',
+      child: IconTheme(
+        data: new IconThemeData(
+          color: Colors.blue,
+        ),
+        child: new Icon(Icons.arrow_forward),
+      ),
+      backgroundColor: Colors.white,
+    ),
+    body: Container(
+        child: Center(
+          child: Text(
+            'Page One',
+            style: TextStyle(fontSize: 35.0, fontWeight: FontWeight.bold, color: Colors.white ),
+          ),
+        )
+    ),
+  );
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    backgroundColor: Colors.amber,
+    floatingActionButton: FloatingActionButton(
+      onPressed: (){
+        Navigator.pop(context);
+      },
+      tooltip:'Next Page',
+      child: IconTheme(
+        data: new IconThemeData(
+          color: Colors.blue,
+        ),
+        child: new Icon(Icons.arrow_back),
+      ),
+      backgroundColor: Colors.white,
+    ),
+    body: Container(
+        child: Center(
+          child: Text(
+            'Page Two',
+            style: TextStyle(fontSize: 35.0, fontWeight: FontWeight.bold, color: Colors.white ),
+          ),
+        )
+    ),
+  );
+}
+
