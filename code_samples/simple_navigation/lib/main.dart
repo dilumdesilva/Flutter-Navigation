57c436653e84ba28c8b159eb4ea5b6c6e3dbc10c
Dilum De Silva <dilumd@zone24x7.com>
2019-06-21 21:15:06 +0530

    Data passing in named routes completed version.

--- code_samples/simple_navigation/lib/main.dart
+++ code_samples/simple_navigation/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:simple_navigation/page1.dart';
+import 'package:simple_navigation/user.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -8,11 +9,81 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+
+        //Declaring Routes
+        routes: <String, WidgetBuilder>{
+          '/page1': (BuildContext context) => new Page1(),
+          '/page2': (BuildContext context) => new Page2(),
+        },
+
+        home: Page1()
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    backgroundColor: Colors.blue,
+    floatingActionButton: FloatingActionButton(
+      onPressed: (){
+        User user = new User(name: 'Dilum');
+        Navigator.of(context).pushNamed('/page2', arguments: user);
+      },
+      tooltip:'Next Page',
+      child: IconTheme(
+        data: new IconThemeData(
+          color: Colors.blue,
+        ),
+        child: new Icon(Icons.arrow_forward),
       ),
-      home: Page1()
+      backgroundColor: Colors.white,
+    ),
+    body: Container(
+        child: Center(
+          child: Text(
+            'Page One',
+            style: TextStyle(fontSize: 35.0, fontWeight: FontWeight.bold, color: Colors.white ),
+          ),
+        )
+    ),
+  );
+}
+
+class Page2 extends StatelessWidget {
+  User user;
+  
+  @override
+  Widget build(BuildContext context) {
+    RouteSettings settings = ModalRoute.of(context).settings;
+    user = settings.arguments;
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.pop(context);
+        },
+        tooltip:'Next Page',
+        child: IconTheme(
+          data: new IconThemeData(
+            color: Colors.blue,
+          ),
+          child: new Icon(Icons.arrow_back),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: Container(
+          child: Center(
+            child: Text(
+              'Hello ${this.user.name}',
+              style: TextStyle(fontSize: 35.0, fontWeight: FontWeight.bold, color: Colors.white ),
+            ),
+          )
+      ),
     );
   }
 }
