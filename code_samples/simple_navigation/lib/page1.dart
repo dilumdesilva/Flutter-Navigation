cde847763b590262213e4776de1cca20e061d0e5
Dilum De Silva <dilumdesilva.info@gmail.com>
2019-06-23 11:46:40 +0530

    Handling back operation using WillPopScope

--- code_samples/simple_navigation/lib/page1.dart
+++ code_samples/simple_navigation/lib/page1.dart
@@ -4,30 +4,48 @@ import 'package:simple_navigation/page2.dart';
 class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
-    backgroundColor: Colors.blue,
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){
-        Navigator.push(context, new MaterialPageRoute(
-          builder: (context) => Page2()
-        ));
-      },
-      tooltip:'Next Page',
-      child: IconTheme(
-        data: new IconThemeData(
-          color: Colors.blue,
+        backgroundColor: Colors.blue,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            var result = await Navigator.push(
+                context, new MaterialPageRoute(builder: (context) => Page2()));
+
+            _navigationResultMsg(context, result);
+          },
+          tooltip: 'Next Page',
+          child: IconTheme(
+            data: new IconThemeData(
+              color: Colors.blue,
+            ),
+            child: new Icon(Icons.arrow_forward),
+          ),
+          backgroundColor: Colors.white,
         ),
-        child: new Icon(Icons.arrow_forward),
-      ),
-      backgroundColor: Colors.white,
-    ),
-    body: Container(
-        child: Center(
+        body: Container(
+            child: Center(
           child: Text(
             'Page One',
-            style: TextStyle(fontSize: 35.0, fontWeight: FontWeight.bold, color: Colors.white ),
+            style: TextStyle(
+                fontSize: 35.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white),
           ),
-        )
-    ),
-  );
-}
+        )),
+      );
 
+  void _navigationResultMsg(BuildContext context, dynamic result) {
+    if (result == 'btn_back') {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text('Navigation has perfomed with back button'),
+              ));
+    } else if (result == 'btn_float') {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Navigation has perfomed with floating back button'),
+          ));
+    }
+  }
+}
--- code_samples/simple_navigation/lib/page2.dart
+++ code_samples/simple_navigation/lib/page2.dart
@@ -2,29 +2,45 @@ import 'package:flutter/material.dart';
 
 class Page2 extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Scaffold(
-    backgroundColor: Colors.amber,
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){
-        Navigator.pop(context);
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      //If you want system to handle this back operation you can pass 'true'
+      //or else if you have your own logic then pass 'false'
+      //onWillPop: () => Future.value(false),
+      onWillPop: () async{
+        _navigatorWithResults(context, 'btn_back');
+        return false;
       },
-      tooltip:'Next Page',
-      child: IconTheme(
-        data: new IconThemeData(
-          color: Colors.blue,
+      child: Scaffold(
+        backgroundColor: Colors.amber,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _navigatorWithResults(context, 'btn_float');
+          },
+          tooltip: 'Next Page',
+          child: IconTheme(
+            data: new IconThemeData(
+              color: Colors.blue,
+            ),
+            child: new Icon(Icons.arrow_back),
+          ),
+          backgroundColor: Colors.white,
         ),
-        child: new Icon(Icons.arrow_back),
-      ),
-      backgroundColor: Colors.white,
-    ),
-    body: Container(
-      child: Center(
-        child: Text(
+        body: Container(
+            child: Center(
+          child: Text(
             'Page Two',
-            style: TextStyle(fontSize: 35.0, fontWeight: FontWeight.bold, color: Colors.white ),
-        ),
-      )
-    ),
-  );
-}
+            style: TextStyle(
+                fontSize: 35.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white),
+          ),
+        )),
+      ),
+    );
+  }
 
+  void _navigatorWithResults(BuildContext context, dynamic result) {
+    Navigator.pop(context, result);
+  }
+}
